74a384b0c10741e04b38bdc251b04a3b8128f3a4
farhan alwanda <raja.farhan.alwanda@gmail.com>
2025-11-19 23:54:17 +0700

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Program sederhana untuk menguji Air
+	fmt.Println("=======================================")
+	fmt.Println("Aplikasi Go BERJALAN via Air!")
+	fmt.Println("=======================================")
+}
